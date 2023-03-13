bcb311aedaeaf39c84c4369c16f0cabd63719aa8
Pedro Zafalon <71289265+ranofrolds@users.noreply.github.com>
2023-03-13 16:43:38 -0300

    Re-organização das pastas app e src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
-import Header from "./components/Header"
-import Login from "./components/Login"
+import Header from "../components/Header"
+import Login from "../components/Login"
 import './styles/style.css'
 
 const inter = Inter({ subsets: ["latin"] });
