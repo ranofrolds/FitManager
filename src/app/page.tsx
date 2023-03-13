d208db8cde38a20bc4028fa3506097dd1b93dc34
Gabriel Biscaia <ra118928@uem.br>
2023-03-13 14:46:02 -0300

    adição de arquivos para estrutura das interfaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import Header from "./components/Header.tsx"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-      <h1>FitManager</h1>
+      <Header />
     </main>
   );
 }
