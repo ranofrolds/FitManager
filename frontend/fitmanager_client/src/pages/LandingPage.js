23f2958ba0532e3144d78bf49699b7d0e74cc539
Gabriel Biscaia <ra118928@uem.br>
2023-03-14 15:59:56 -0300

    atualização da tela inicial'

--- frontend/fitmanager_client/src/pages/LandingPage.js
+++ frontend/fitmanager_client/src/pages/LandingPage.js
@@ -1,7 +1,20 @@
+import "../styles/style.css";
+
 export default function LandingPage(props) {
   return (
-    <div>
-      <h1>FitManager</h1>
+    <div id="landing-div">
+      <h1 id="titulo">FitManager</h1>
+      <h2 id="subtitulo">
+        Bem-vindo ao gerenciador do aplicativo do FitManager
+      </h2>
+      <div id="botoes">
+        <button type="submit" id="entrar">
+          Entrar
+        </button>
+        <button type="submit" id="cadastrar">
+          Cadastrar
+        </button>
+      </div>
     </div>
   );
 }
