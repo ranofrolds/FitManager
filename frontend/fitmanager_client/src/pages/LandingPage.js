dd050ed0409bf703dc70b35e29318644b2f38612
Gabriel Biscaia <ra118928@uem.br>
2023-03-15 18:37:13 -0300

    atualizando a landing page para tela de login

--- frontend/fitmanager_client/src/pages/LandingPage.js
+++ frontend/fitmanager_client/src/pages/LandingPage.js
@@ -3,18 +3,38 @@ import "../styles/style.css";
 export default function LandingPage(props) {
   return (
     <div id="landing-div">
-      <h1 id="titulo">FitManager</h1>
-      <h2 id="subtitulo">
+      <h1 id="title">FitManager</h1>
+      <h2 id="subtitle">
         <span>Bem</span>-<span>vindo</span> ao gerenciador do aplicativo do
         FitManager
       </h2>
-      <div id="botoes">
-        <button type="submit" id="entrar">
-          Entrar
-        </button>
-        <button type="submit" id="cadastrar">
-          Cadastrar
-        </button>
+      <div id="container-login">
+        <h2>Login</h2>
+        <form action="#">
+          <div class="input-box">
+            <span class="icon">
+              <ion-icon name="mail-outline"></ion-icon>
+            </span>
+            <input type="email" required />
+            <label>Email</label>
+          </div>
+          <div class="input-box">
+            <span class="icon">
+              <ion-icon name="lock-closed-outline"></ion-icon>
+            </span>
+            <input type="password" required />
+            <label>Senha</label>
+          </div>
+          <div class="lembrar-senha">
+            <label>
+              <input type="checkbox" /> Lembre-se de mim
+            </label>
+            <a href="#">Esqueceu a senha?</a>
+          </div>
+          <button type="submit" class="botao">
+            Entrar
+          </button>
+        </form>
       </div>
     </div>
   );
