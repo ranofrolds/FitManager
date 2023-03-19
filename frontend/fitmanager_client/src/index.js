f72d821c01c397a9ab4365b208c5364accf592d5
Gabriel Biscaia <ra118928@uem.br>
2023-03-19 18:31:22 -0300

    adição arquivos index.jsx e App.jsx

--- frontend/fitmanager_client/src/index.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./styles/style.css";
-import Login from "./pages/Login";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(<Login />);
