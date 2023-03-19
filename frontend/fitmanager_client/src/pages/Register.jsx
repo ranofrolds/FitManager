a63352d478cae467cb3ee6d8a99068b8910682ac
Gabriel Biscaia <ra118928@uem.br>
2023-03-19 18:23:22 -0300

    alteração de pastas, uma pasta para cada pagina

--- frontend/fitmanager_client/src/pages/Register.jsx
+++
@@ -1,5 +0,0 @@
-export default function Register() {
-  <div id="main-div">
-    <h2>Cadastro</h2>
-  </div>;
-}
---
+++ frontend/fitmanager_client/src/pages/Register/Register.jsx
@@ -0,0 +1,7 @@
+export const Register = () => {
+  return (
+    <div id="main-div">
+      <h2>Cadastro</h2>
+    </div>
+  );
+};
