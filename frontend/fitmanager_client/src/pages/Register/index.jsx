2f54c01513a116a3055f95d4ea27fb076e0c4877
Gabriel Biscaia <ra118928@uem.br>
2023-03-19 22:33:37 -0300

    Atualização tela de cadastro

--- frontend/fitmanager_client/src/pages/Register/index.jsx
+++ frontend/fitmanager_client/src/pages/Register/index.jsx
@@ -11,9 +11,53 @@ export const Register = () => {
   const [password, setPassword] = useState("");
 
   return (
-      <div id="main-div">
-        <Header />
+    <div id="main-div">
+      <Header />
+      <div id="container-login">
         <h2>Cadastro</h2>
+        <form action="#">
+          <div class="input-box">
+            <span class="icon">
+              <ion-icon name="storefront-outline"></ion-icon>
+            </span>
+            <input type="text" required />
+            <label>Nome da Academia</label>
+          </div>
+
+          <div class="input-box">
+            <span class="icon">
+              <ion-icon name="business-outline"></ion-icon>
+            </span>
+            <input type="text" required />
+            <label>CNPJ</label>
+          </div>
+
+          <div class="input-box">
+            <span class="icon">
+              <ion-icon name="mail-outline"></ion-icon>
+            </span>
+            <input type="email" required />
+            <label>Email</label>
+          </div>
+
+          <div class="input-box">
+            <span class="icon">
+              <ion-icon name="lock-closed-outline"></ion-icon>
+            </span>
+            <input type="password" required />
+            <label>Senha</label>
+          </div>
+
+          <button type="submit" class="botao">
+            Cadastrar
+          </button>
+          <div id="register-div">
+            <p>
+              <Link to="/"> Ja possuo uma conta </Link>
+            </p>
+          </div>
+        </form>
       </div>
+    </div>
   );
 };
