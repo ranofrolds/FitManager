7e432c7b1988ffd697f76003baad4aad23b36132
vinigondo <viniciusgondo@gmail.com>
2023-03-20 00:27:59 -0300

    texto de alteração

---
+++ frontend/fitmanager_client/src/pages/Grafico/Grafico.ts
@@ -0,0 +1,25 @@
+import Chart from 'chart.js';
+
+const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+const myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'Sales',
+      data: [12, 19, 3, 5, 2, 3, 15],
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
