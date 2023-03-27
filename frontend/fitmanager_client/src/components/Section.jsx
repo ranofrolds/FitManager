f0c82b8300ef3d46610ffe6ff4ebfab9801e56f6
Gabriel Biscaia <ra118928@uem.br>
2023-03-27 16:39:47 -0300

    adição do componente Section

---
+++ frontend/fitmanager_client/src/components/Section.jsx
@@ -0,0 +1,10 @@
+import "../styles/style.css";
+
+export default function Section(props) {
+  return (
+    <div class="section-div">
+      <h1 class="section-title">Titulo</h1>
+      <h2 class="section-subtitle">Subtitulo</h2>
+    </div>
+  );
+}
--- frontend/fitmanager_client/src/pages/Home/index.jsx
+++ frontend/fitmanager_client/src/pages/Home/index.jsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
-import Header from "../../components/Header";
 import axios from "axios";
 
+import Header from "../../components/Header";
+import Section from "../../components/Section";
+
+import "../../styles/style.css";
+
 export const Home = () => {
-  return <Header />;
+  return (
+    <div id="main-div">
+      <Header />
+      <Section />
+    </div>
+  );
 };
