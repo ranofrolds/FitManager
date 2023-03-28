4378920de2b1f13331e117d9f8ef0f0a39a9ea95
Gabriel Biscaia <ra118928@uem.br>
2023-03-28 00:59:58 -0300

    Adição de rota de funcionarios

--- frontend/fitmanager_client/src/routes/index.jsx
+++ frontend/fitmanager_client/src/routes/index.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
-import { Home } from "../pages/Home"
+import { Home } from "../pages/Home";
+import { Workers } from "../pages/Workers";
 
 export const AppRouter = () => {
   return (
@@ -11,6 +12,7 @@ export const AppRouter = () => {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/workers" element={<Workers />} />
       </Routes>
     </Router>
   );
