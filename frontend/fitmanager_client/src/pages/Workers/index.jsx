3876db9e3be7df3942017565f276abb09edfd2f6
Gabriel Biscaia <ra118928@uem.br>
2023-03-28 01:00:40 -0300

    Adição de novas paginas

---
+++ frontend/fitmanager_client/src/pages/Workers/index.jsx
@@ -0,0 +1,19 @@
+import Section from "../../components/Section";
+import Header from "../../components/Header";
+
+import "../../styles/style.css";
+
+export const Workers = () => {
+  return (
+    <div id="main-div">
+      <Header />
+      <h3>Funcionários</h3>
+      <div class="section-grid">
+        <Section title="Limpeza" />
+        <Section title="Personais" />
+        <Section title="Administração" />
+        <Section title="Manuteção" />
+      </div>
+    </div>
+  );
+};
