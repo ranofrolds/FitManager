9fdeb4b6830810e355cd4cb326187c54788a9474
Pedro Zafalon <71289265+ranofrolds@users.noreply.github.com>
2023-03-28 16:28:45 -0300

    refatoração na logica de rotas com restricao de login

--- frontend/fitmanager_client/src/pages/Login/index.jsx
+++ frontend/fitmanager_client/src/pages/Login/index.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { Navigate } from 'react-router'
 import { useState } from "react";
 import Header from "../../components/Header.jsx";
 import axios from "axios";
@@ -11,91 +10,85 @@ export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const token = Cookies.get('auth_token');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8000/auth/login", {
+        email: email,
+        password:password,
+      })
+      .then((res) => {
+        const thirtyMinutes = 30 * 60 * 1000; // em milissegundos
+        const expirationDate = new Date(Date.now() + thirtyMinutes);
 
-  if(token){
-    return <Navigate to="/home"/>;
-  }
-  else{
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      axios
-        .post("http://localhost:8000/auth/login", {
-          email: email,
-          password:password,
-        })
-        .then((res) => {
-          const thirtyMinutes = 30 * 60 * 1000; // em milissegundos
-          const expirationDate = new Date(Date.now() + thirtyMinutes);
+        const token = res.data.token;
 
-          const token = res.data.token;
+        Cookies.set('auth_token', token, { expires: expirationDate });
 
-          Cookies.set('auth_token', token, { expires: expirationDate });
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-          console.log(res);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-
-    return (
-      <div id="main-div">
-        <Header />
-        <h2 id="subtitle">
-          <span>Bem</span>-<span>vindo</span> ao seu sistema de gerenciamento de
-          academias!
-        </h2>
-        <div id="container-login">
-          <h2>Login</h2>
-          <form onSubmit={handleSubmit} action="#">
-            <div class="input-box">
-              <span class="icon">
-                <ion-icon name="mail-outline"></ion-icon>
-              </span>
-              <input
-                className={email !== "" ? "has-val input" : "input"}
-                type="email"
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <label>Email</label>
-            </div>
-            <div class="input-box">
-              <span class="icon">
-                <ion-icon name="lock-closed-outline"></ion-icon>
-              </span>
-              <input
-                className={password !== "" ? "has-val input" : "input"}
-                type="password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <label>Senha</label>
-            </div>
-            <div class="remember-forgot">
-              <label>
-                <input type="checkbox" /> Lembre-se de mim
-              </label>
-              <a href>Esqueceu a senha?</a>
-            </div>
-            <button type="submit" class="botao">
-              <Link class="escrita-botao">
-                Entrar
-              </Link>
-            </button>
-            <div id="register-div">
-              <p>
-                Não tem uma conta? <Link to="./register"> Cadastre-se </Link>
-              </p>
-            </div>
-          </form>
-        </div>
+  return (
+    <div id="main-div">
+      <Header />
+      <h2 id="subtitle">
+        <span>Bem</span>-<span>vindo</span> ao seu sistema de gerenciamento de
+        academias!
+      </h2>
+      <div id="container-login">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit} action="#">
+          <div class="input-box">
+            <span class="icon">
+              <ion-icon name="mail-outline"></ion-icon>
+            </span>
+            <input
+              className={email !== "" ? "has-val input" : "input"}
+              type="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Email</label>
+          </div>
+          <div class="input-box">
+            <span class="icon">
+              <ion-icon name="lock-closed-outline"></ion-icon>
+            </span>
+            <input
+              className={password !== "" ? "has-val input" : "input"}
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label>Senha</label>
+          </div>
+          <div class="remember-forgot">
+            <label>
+              <input type="checkbox" /> Lembre-se de mim
+            </label>
+            <a href>Esqueceu a senha?</a>
+          </div>
+          <button type="submit" class="botao">
+            <Link class="escrita-botao">
+              Entrar
+            </Link>
+          </button>
+          <div id="register-div">
+            <p>
+              Não tem uma conta? <Link to="./register"> Cadastre-se </Link>
+            </p>
+          </div>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
+  
 
   
 };
