b99a4221eaad63de40a6dc19051c8e9ff881df39
Gabriel Biscaia <ra118928@uem.br>
2023-03-28 01:08:27 -0300

    adição da página de alunos

---
+++ frontend/fitmanager_client/src/pages/Customers/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header";
+
+import "../../styles/style.css";
+
+export const Customers = () => {
+  return (
+    <div id="main-div">
+      <Header />
+      <h3>Alunos</h3>
+    </div>
+  );
+};
