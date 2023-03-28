93c4de82d27ce72130e163b2f5e7721d2e860d14
Gabriel Biscaia <ra118928@uem.br>
2023-03-28 01:08:58 -0300

    adição de novas rotas

--- frontend/fitmanager_client/src/routes/index.jsx
+++ frontend/fitmanager_client/src/routes/index.jsx
@@ -4,6 +4,7 @@ import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { Home } from "../pages/Home";
 import { Workers } from "../pages/Workers";
+import { Customers } from "../pages/Customers";
 
 export const AppRouter = () => {
   return (
@@ -12,6 +13,8 @@ export const AppRouter = () => {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/workers" element={<Workers />} />
+        <Route path="/customers" element={<Customers />} />
         <Route path="/workers" element={<Workers />} />
       </Routes>
     </Router>
