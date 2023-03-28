1fb9b50720d5d8863950f0fe1e617987ff0e6f5d
Gabriel Biscaia <ra118928@uem.br>
2023-03-28 00:56:54 -0300

    atualização do Header, ao clicar nele voce vai para o home

--- frontend/fitmanager_client/src/components/Header.jsx
+++ frontend/fitmanager_client/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import fitmanagerLogo from "../assets/logo-fitmanager.png";
+import { Link } from "react-router-dom";
+
 import "../styles/style.css";
 
 export default function Header(props) {
   return (
     <div id="header">
-      <h1 id="title">FitManager</h1>
+      <h1 id="title">
+        <Link to="/home">FitManager</Link>
+      </h1>
       <img id="fitmanager-logo" src={fitmanagerLogo} alt="logo-fitmanager" />
     </div>
   );
