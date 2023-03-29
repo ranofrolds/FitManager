e2ad7e5d9b3b72b9c5e651abc861f0d55b22a56c
Arthur Maciel <ra120119@uem.br>
2023-03-28 22:32:07 -0300

    read do funcionario feito

--- backend/fitmanager_backend/src/services/funcionarios.service.ts
+++ backend/fitmanager_backend/src/services/funcionarios.service.ts
@@ -1,9 +1,10 @@
-import { Injectable} from '@nestjs/common';
+import { Injectable, NotFoundException} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Funcionario } from '../models/funcionarios.model';
 import * as bcrypt from 'bcrypt';
 import { FuncionarioDto } from '../dto/funcionarios.dto';
+import { IFuncionario } from 'src/interface/funcionarios.interface';
 
 @Injectable()
 export class FuncionarioService {
@@ -17,7 +18,7 @@ export class FuncionarioService {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await this.FuncionarioModel.create({
+    const funcionario = await this.FuncionarioModel.create({
       cpf,
       name,
       email,
@@ -29,4 +30,14 @@ export class FuncionarioService {
       frequency
     });
   }
+
+  async getFuncionarios():Promise<IFuncionario[]>{
+    const funcionarioData = await this.FuncionarioModel.find()
+    if(!funcionarioData || funcionarioData.lenght == 0){
+      throw new NotFoundException("Funcionario data not found")
+    }
+    return funcionarioData
+  }
+  
+  
 }
