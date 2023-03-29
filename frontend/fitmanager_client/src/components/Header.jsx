d837afac6ef13865a618a9653f80b9c7ed0fb8c6
Gabriel Biscaia <ra118928@uem.br>
2023-03-29 00:52:15 -0300

    ultima atualização da segunda sprint gabriel

--- frontend/fitmanager_client/src/components/Header.jsx
+++ frontend/fitmanager_client/src/components/Header.jsx
@@ -7,7 +7,9 @@ export default function Header(props) {
   return (
     <div id="header">
       <h1 id="title">
-        <Link to="/home">FitManager</Link>
+        <Link to="/home" className="text-white">
+          FitManager
+        </Link>
       </h1>
       <img id="fitmanager-logo" src={fitmanagerLogo} alt="logo-fitmanager" />
     </div>
