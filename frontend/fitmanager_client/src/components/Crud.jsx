58f81d15aa4252ef22fa0b4e0f0b3dab49dc2e25
Gabriel Biscaia <ra118928@uem.br>
2023-04-16 19:54:02 -0300

    atualização dos dados do CRUD

--- frontend/fitmanager_client/src/components/Crud.jsx
+++ frontend/fitmanager_client/src/components/Crud.jsx
@@ -1,140 +1,93 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+} from "@chakra-ui/react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
-import axiosInstance from "../instances/axiosInstances";
 
-import "../styles/style.css";
+const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
+  const [name, setName] = useState(dataEdit.name || "");
+  const [email, setEmail] = useState(dataEdit.email || "");
 
-export default function Crud(props) {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [phone, setPhone] = useState("");
-  const [instructor, setInstructor] = useState("");
-  const [plan, setPlan] = useState("");
-  const navigate = useNavigate();
+  const handleSave = () => {
+    if (!name || !email) return;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axiosInstance
-      .post("/api/exercise", {
-        name: name,
-        age: age,
-        phone: phone,
-        instructor: instructor,
-        plan: plan,
-      })
-      .then((response) => {
-        console.log(response.data);
-        navigate("/customers");
-      })
-      .catch((error) => {
-        console.log(error.response.data);
-      });
+    if (emailAlreadyExists()) {
+      return alert("E-mail já cadastrado!");
+    }
+
+    if (Object.keys(dataEdit).length) {
+      data[dataEdit.index] = { name, email };
+    }
+
+    const newDataArray = !Object.keys(dataEdit).length
+      ? [...(data ? data : []), { name, email }]
+      : [...(data ? data : [])];
+
+    localStorage.setItem("cad_cliente", JSON.stringify(newDataArray));
+
+    setData(newDataArray);
+
+    onClose();
+  };
+
+  const emailAlreadyExists = () => {
+    if (dataEdit.email !== email && data?.length) {
+      return data.find((item) => item.email === email);
+    }
+
+    return false;
   };
 
   return (
-    <div class="crud-div">
-      <button type="button" data-toggle="modal" data-target="#myModal">
-        Launch modal
-      </button>
-      <div class="modal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title">{props.title}</h5>
-              <button
-                type="button"
-                class="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <form onSubmit={handleSubmit} action="#">
-                <div class="input-box">
-                  <span class="icon">
-                    <ion-icon name="text-outline"></ion-icon>
-                  </span>
-                  <input
-                    className={name !== "" ? "has-val input" : "input"}
-                    type="text"
-                    required
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                  <label>Nome</label>
-                </div>
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Cadastro de Clientes</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl display="flex" flexDir="column" gap={4}>
+              <Box>
+                <FormLabel>Nome</FormLabel>
+                <Input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Box>
+              <Box>
+                <FormLabel>E-mail</FormLabel>
+                <Input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Box>
+            </FormControl>
+          </ModalBody>
 
-                <div class="input-box">
-                  <span class="icon">
-                    <ion-icon name="today-outline"></ion-icon>
-                  </span>
-                  <input
-                    className={age !== "" ? "has-val input" : "input"}
-                    type="number"
-                    required
-                    value={age}
-                    onChange={(e) => setAge(e.target.value)}
-                  />
-                  <label>Idade</label>
-                </div>
-
-                <div class="input-box">
-                  <span class="icon">
-                    <ion-icon name="call-outline"></ion-icon>
-                  </span>
-                  <input
-                    className={phone !== "" ? "has-val input" : "input"}
-                    type="string"
-                    required
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                  />
-                  <label>Celular</label>
-                </div>
-
-                <div class="input-box">
-                  <span class="icon">
-                    <ion-icon name="person-outline"></ion-icon>
-                  </span>
-                  <input
-                    className={instructor !== "" ? "has-val input" : "input"}
-                    type="string"
-                    required
-                    value={instructor}
-                    onChange={(e) => setInstructor(e.target.value)}
-                  />
-                  <label>Instrutor</label>
-                </div>
-
-                <div class="input-box">
-                  <span class="icon">
-                    <ion-icon name="cash-outline"></ion-icon>
-                  </span>
-                  <input
-                    className={plan !== "" ? "has-val input" : "input"}
-                    type="string"
-                    required
-                    value={plan}
-                    onChange={(e) => setPlan(e.target.value)}
-                  />
-                  <label>Plano</label>
-                </div>
-              </form>
-            </div>
-            <div class="modal-footer">
-              <button type="submit" id="save-button" onClick={handleSubmit}>
-                Salvar
-              </button>
-              <button type="submit" id="cancel-button">
-                Cancelar
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+          <ModalFooter justifyContent="start">
+            <Button colorScheme="green" mr={3} onClick={handleSave}>
+              SALVAR
+            </Button>
+            <Button colorScheme="red" onClick={onClose}>
+              CANCELAR
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
-}
+};
+
+export default ModalComp;
--- frontend/fitmanager_client/src/pages/Customers/index.jsx
+++ frontend/fitmanager_client/src/pages/Customers/index.jsx
@@ -1,17 +1,165 @@
 import Header from "../../components/Header";
 import Logout from "../../components/Logout";
 import Crud from "../../components/Crud";
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Button,
+  useDisclosure,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 import "../../styles/style.css";
 
 export const Customers = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [data, setData] = useState([]);
+  const [dataEdit, setDataEdit] = useState({});
+
+  useEffect(() => {
+    const db_costumer = localStorage.getItem("cad_cliente")
+      ? JSON.parse(localStorage.getItem("cad_cliente"))
+      : [];
+
+    setData(db_costumer);
+  }, [setData]);
+
+  const handleRemove = (email) => {
+    const newArray = data.filter((item) => item.email !== email);
+
+    setData(newArray);
+
+    localStorage.setItem("cad_cliente", JSON.stringify(newArray));
+  };
+
   return (
     <div id="main-div">
       <Header />
       <Logout />
       <div class="white-box">
-        <h3>Alunos</h3>
-        <Crud title="Listagem de alunos" />
+        <Flex>
+          <Box maxW={800} w="100%" h="100vh" py={10} px={2}>
+            <Button
+              colorScheme="blue"
+              onClick={() => [setDataEdit({}), onOpen()]}
+            >
+              NOVO CADASTRO
+            </Button>
+
+            <Box overflowY="auto" height="100%">
+              <Table mt="6">
+                <Thead>
+                  <Tr>
+                    <Th maxW={300} fontSize="16px">
+                      CPF
+                    </Th>
+                    <Th maxW={300} fontSize="16px">
+                      EMAIL
+                    </Th>
+                    <Th maxW={300} fontSize="16px">
+                      PHONE
+                    </Th>
+                    <Th maxW={300} fontSize="16px">
+                      ACADEMIA
+                    </Th>
+                    <Th maxW={300} fontSize="16px">
+                      NOME
+                    </Th>
+                    <Th maxW={300} fontSize="16px">
+                      SENHA
+                    </Th>
+                    <Th maxW={300} fontSize="16px">
+                      PLANO
+                    </Th>
+                    <Th maxW={300} fontSize="16px">
+                      PROFESSOR
+                    </Th>
+                    <Th maxW={300} fontSize="16px">
+                      DATA NASCIMENTO
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map(
+                    (
+                      {
+                        cpf,
+                        email,
+                        phone,
+                        academia,
+                        name,
+                        password,
+                        plano,
+                        professor,
+                        dataNascimento,
+                      },
+                      index
+                    ) => (
+                      <Tr
+                        key={index}
+                        cursor="pointer "
+                        _hover={{ bg: "gray.100" }}
+                      >
+                        <Td maxW={300}>{cpf}</Td>
+                        <Td maxW={300}>{email}</Td>
+                        <Td maxW={300}>{phone}</Td>
+                        <Td maxW={300}>{academia}</Td>
+                        <Td maxW={300}>{name}</Td>
+                        <Td maxW={300}>{password}</Td>
+                        <Td maxW={300}>{plano}</Td>
+                        <Td maxW={300}>{professor}</Td>
+                        <Td maxW={300}>{dataNascimento}</Td>
+
+                        <EditIcon
+                          fontSize={20}
+                          onClick={() => [
+                            setDataEdit({
+                              cpf,
+                              email,
+                              phone,
+                              academia,
+                              name,
+                              password,
+                              plano,
+                              professor,
+                              dataNascimento,
+                              index,
+                            }),
+                            onOpen(),
+                          ]}
+                        />
+
+                        <Td p={0}>
+                          <DeleteIcon
+                            fontSize={20}
+                            onClick={() => handleRemove(email)}
+                          />
+                        </Td>
+                      </Tr>
+                    )
+                  )}
+                </Tbody>
+              </Table>
+            </Box>
+          </Box>
+          {isOpen && (
+            <Crud
+              isOpen={isOpen}
+              onClose={onClose}
+              data={data}
+              setData={setData}
+              dataEdit={dataEdit}
+              setDataEdit={setDataEdit}
+            />
+          )}
+        </Flex>
       </div>
     </div>
   );
